743f188da57cc04be24bae1d308b21c50ad3e7d2
Lolodin <serlolodin@gmail.com>
2021-11-21 15:08:03 +0300

    add action aicon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,9 +8,23 @@ export const Header = () => {
         <header className={ style.header }>
             <div className={ style.header__container }>
                 <div className={ style.header_row }>
-                    <div className={ style.header_left }></div>
+                    <div className={ style.header_left }>
+                        <div className={ style.header__days }>
+                            <span  className={ style["header__days-icon"] }/>
+                            19
+                        </div>
+                        <div className={ style.header__money }>
+                            <span className={ style["header__money-icon"] }/>
+                            20
+                        </div>
+                    </div>
                     <div className={ style.header_center }>Merchant</div>
-                    <div className={ style.header_right }></div>
+                    <div className={ style.header_right }>
+                        <div className={ style.header__bag }>
+                            <span className={ style["header__bag-icon"] }/>
+                            0/10
+                        </div>
+                    </div>
                 </div>
             </div>
         </header>
--- src/components/sidebarTraders/SidebarTraders.jsx
+++ src/components/sidebarTraders/SidebarTraders.jsx
@@ -15,17 +15,29 @@ export const SidebarTraders = () => {
                         <div className={ style.traders__item }>
                             <div className={ style.traders__avatar }></div>
                             <h3 className={ style.traders__title }> Trader </h3>
-                            <p  className={ style.traders__discription }></p>
+                            <p className={ style.traders__discription }></p>
+                            <div className={ style.traders__actions }>
+                                <button className={ style["traders__actions-buy"] }>Byu</button>
+                                <button className={ style["traders__actions-sell"] }>Sell</button>
+                            </div>
                         </div>
                         <div className={ style.traders__item }>
                             <div className={ style.traders__avatar }></div>
                             <h3 className={ style.traders__title }> Trader </h3>
                             <p  className={ style.traders__discription }></p>
+                            <div className={ style.traders__actions }>
+                                <button className={ style["traders__actions-buy"] }>Byu</button>
+                                <button className={ style["traders__actions-sell"] }>Sell</button>
+                            </div>
                         </div>
                         <div className={ style.traders__item }>
                             <div className={ style.traders__avatar }></div>
                             <h3 className={ style.traders__title }> Trader </h3>
                             <p  className={ style.traders__discription }></p>
+                            <div className={ style.traders__actions }>
+                                <button className={ style["traders__actions-buy"] }>Byu</button>
+                                <button className={ style["traders__actions-sell"] }>Sell</button>
+                            </div>
                         </div>
                     </div>
                 </div>
